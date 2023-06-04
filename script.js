d0ba37036b0893708bb78fbd73976ffa9258c8ff
Inno Paolo <102833497+innopaolo@users.noreply.github.com>
2023-06-05 00:41:27 +0600

    Switch position of  canvas icons

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+
+
+
 //This Draggable constructor creates the ability to rotate the rounded controllers
 const sizeController = Draggable.create("#size-controller",{
     type: "rotation",
     bounds:{minRotation: 270, maxRotation: 360},
-    throwProps: true,
     onDragEnd: () => {
         sizeController[0].endRotation > 135 ? selectedSize = 32 : selectedSize = 16;        
         eraseGrid();
